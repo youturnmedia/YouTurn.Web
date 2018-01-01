e3f233d62e7e6b911e7e42df425f3bb8b90e0921
Senthil Sivanath <Senthil.Sivanath@tesco.com>
2018-01-01 10:03:31 +0530

    Added heroku and bootstrap

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -5,6 +5,7 @@ export default class App extends Component {
 
   render() {
     return <div>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
       <Helmet title='Go + React + Redux = rocks!' />
       {this.props.children}
     </div>;
